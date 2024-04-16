06d1a8838a9ed80d428f4320c44384d255903758
kvrmuruganand <167224555+kvrmuruganand@users.noreply.github.com>
2024-04-16 20:48:43 +0530

    subnets updated

---
+++ vpc.tf
@@ -0,0 +1,39 @@
+module "label_vpc" {
+  source     = "cloudposse/label/null"
+  version    = "0.25.0"
+  context    = module.base_label.context
+  name       = "vpc"
+  attributes = ["main"]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = module.label_vpc.tags
+}
+
+# =========================
+# Create your subnets here
+# =========================
+resource "aws_subnet" "public_subnets" {
+ count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.public_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+
+ tags = {
+   Name = "Public Subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
